033c9b3548c9175db7a63650178948c76b6a30a4
董萍 <dp@zhuozhengsoft.com>
2022-08-31 16:19:09 +0800

    修改ReadOnly示例，去掉setAllowCopy，使用禁止右键和禁止选中替代

--- samples-springboot-back/src/main/java/com/zhuozhengsoft/samplesspringbootback/controller/ReadOnlyController.java
+++ samples-springboot-back/src/main/java/com/zhuozhengsoft/samplesspringbootback/controller/ReadOnlyController.java
@@ -2,6 +2,7 @@ package com.zhuozhengsoft.samplesspringbootback.controller;
 
 import com.zhuozhengsoft.pageoffice.OpenModeType;
 import com.zhuozhengsoft.pageoffice.PageOfficeCtrl;
+import com.zhuozhengsoft.pageoffice.wordwriter.WordDocument;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,9 +17,12 @@ public class ReadOnlyController {
 
     @RequestMapping(value = "/Word")
     public String showWord(HttpServletRequest request) {
+		WordDocument wordDoc=new WordDocument();
+        wordDoc.setDisableWindowSelection(true);//禁止选中
+        wordDoc.setDisableWindowRightClick(true);//禁止右键
+
         PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
         poCtrl.setServerPage("/api/poserver.zz");//设置服务页面
-        poCtrl.setAllowCopy(false);//禁止拷贝
         poCtrl.setMenubar(false);//隐藏菜单栏
         poCtrl.setOfficeToolbars(false);//隐藏Office工具条
         poCtrl.setCustomToolbar(false);//隐藏自定义工具栏
@@ -26,6 +30,7 @@ public class ReadOnlyController {
         //设置页面的显示标题
         poCtrl.setCaption("演示：文件在线安全浏览");
         //打开Word文档
+	    poCtrl.setWriter(wordDoc);//此句必须
         poCtrl.webOpen("/api/doc/ReadOnly/test.doc", OpenModeType.docNormalEdit, "张三");
         return poCtrl.getHtmlCode("PageOfficeCtrl1");
     }
--- samples-springboot-back/src/main/java/com/zhuozhengsoft/samplesspringbootback/controller/SimpleWordController.java
+++ samples-springboot-back/src/main/java/com/zhuozhengsoft/samplesspringbootback/controller/SimpleWordController.java
@@ -19,49 +19,49 @@ import javax.servlet.http.HttpServletResponse;
 @RestController
 @RequestMapping(value = "/SimpleWord")
 public class SimpleWordController {
-        //获取doc目录的磁盘路径
-        private String dir = GetDirPathUtil.getDirPath() + "static/doc/";
+    //获取doc目录的磁盘路径
+    private String dir = GetDirPathUtil.getDirPath() + "static/doc/";
 
-        @RequestMapping(value="/Word")
-        @ResponseBody
-        public String showWord(HttpServletRequest request) {
-            PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
-            poCtrl.setServerPage("/api/poserver.zz");//设置服务页面
-            poCtrl.addCustomToolButton("保存", "Save", 1);
-            poCtrl.addCustomToolButton("另存为", "SaveAs", 12);
-            poCtrl.addCustomToolButton("打印设置", "PrintSet", 0);
-            poCtrl.addCustomToolButton("打印", "PrintFile", 6);
-            poCtrl.addCustomToolButton("全屏/还原", "IsFullScreen", 4);
-            poCtrl.addCustomToolButton("-", "", 0);
-            poCtrl.addCustomToolButton("关闭", "Close", 21);
-            poCtrl.setSaveFilePage("/api/SimpleWord/save");//设置保存方法的url
-            //打开word
-            poCtrl.webOpen("/api/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit, "张三");
-            return  poCtrl.getHtmlCode("PageOfficeCtrl1");
-        }
-
-        @RequestMapping(value ="/Word1")
-        @ResponseBody
-        public String showWord1(HttpServletRequest request) {
-            PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
-            poCtrl.setServerPage("/api/poserver.zz");//设置服务页面
-            //添加自定义按钮
-            poCtrl.addCustomToolButton("保存", "Save", 1);
-            //设置保存页面
-            poCtrl.setSaveFilePage("/api/SimpleWord/save");//设置处理文件保存的请求方法
-            //打开Word文档
-            //打开磁盘路径的话有两种写法：1.D:\\doc\\test.doc  2.file://D:/doc/test.doc
-            poCtrl.webOpen("file://"+dir+"/SimpleWord/test.doc", OpenModeType.docNormalEdit, "张三");
-            return poCtrl.getHtmlCode("PageOfficeCtrl1");
-        }
-
-        @RequestMapping("save")
-        public void save(HttpServletRequest request, HttpServletResponse response) {
-            FileSaver fs = new FileSaver(request, response);
-            fs.saveToFile(dir + "SimpleWord/" + fs.getFileName());
-            fs.close();
-        }
-
-
+    @RequestMapping(value="/Word")
+    @ResponseBody
+    public String showWord(HttpServletRequest request) {
+        PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
+        poCtrl.setServerPage("/api/poserver.zz");//设置服务页面
+        poCtrl.addCustomToolButton("保存", "Save", 1);
+        poCtrl.addCustomToolButton("另存为", "SaveAs", 12);
+        poCtrl.addCustomToolButton("打印设置", "PrintSet", 0);
+        poCtrl.addCustomToolButton("打印", "PrintFile", 6);
+        poCtrl.addCustomToolButton("全屏/还原", "IsFullScreen", 4);
+        poCtrl.addCustomToolButton("-", "", 0);
+        poCtrl.addCustomToolButton("关闭", "Close", 21);
+        poCtrl.setSaveFilePage("/api/SimpleWord/save");//设置保存方法的url
+        //打开word
+        poCtrl.webOpen("/api/doc/SimpleWord/test.doc", OpenModeType.docNormalEdit, "张三");
+        return  poCtrl.getHtmlCode("PageOfficeCtrl1");
     }
+
+    @RequestMapping(value ="/Word1")
+    @ResponseBody
+    public String showWord1(HttpServletRequest request) {
+        PageOfficeCtrl poCtrl = new PageOfficeCtrl(request);
+        poCtrl.setServerPage("/api/poserver.zz");//设置服务页面
+        //添加自定义按钮
+        poCtrl.addCustomToolButton("保存", "Save", 1);
+        //设置保存页面
+        poCtrl.setSaveFilePage("/api/SimpleWord/save");//设置处理文件保存的请求方法
+        //打开Word文档
+        //打开磁盘路径的话有两种写法：1.D:\\doc\\test.doc  2.file://D:/doc/test.doc
+        poCtrl.webOpen("file://"+dir+"/SimpleWord/test.doc", OpenModeType.docNormalEdit, "张三");
+        return poCtrl.getHtmlCode("PageOfficeCtrl1");
+    }
+
+    @RequestMapping("save")
+    public void save(HttpServletRequest request, HttpServletResponse response) {
+        FileSaver fs = new FileSaver(request, response);
+        fs.saveToFile(dir + "SimpleWord/" + fs.getFileName());
+        fs.close();
+    }
+
+
+}
 
